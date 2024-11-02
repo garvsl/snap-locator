45ed5ef50c89f09b094170d19cf75def53da3060
garvsl <garvsehgal7@gmail.com>
2024-11-01 23:53:23 -0400

    export required methods

--- src/index.js
+++ src/index.js
@@ -1,1 +1,4 @@
-export { default as fetchData } from "./fetchData.js";
+export { retrieve } from "./retrieve.js";
+export { fetchData } from "./fetchData.js";
+export { locationDetails } from "./locationDetails.js";
+export { suggestLocations } from "./suggestLocations.js";
